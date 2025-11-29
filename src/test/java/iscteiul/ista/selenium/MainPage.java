b73790ff943425cc944af58595a7db6e12a6203c
Rafael Costa <racas1@iscte-iul.pt>
2025-11-29 11:13:56 +0000

    122645 Branch

--- src/test/java/iscteiul/ista/selenium/MainPage.java
+++ src/test/java/iscteiul/ista/selenium/MainPage.java
@@ -1,13 +1,28 @@
 package iscteiul.ista.selenium;
-import com.codeborne.selenide.SelenideElement;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
 
 // page_url = https://www.jetbrains.com/
 public class MainPage {
-  public SelenideElement seeDeveloperToolsButton = $x("//*[@data-test-marker='Developer Tools']");
-  public SelenideElement findYourToolsButton = $x("//*[@data-test='suggestion-action']");
-  public SelenideElement toolsMenu = $x("//div[@data-test='main-menu-item' and @data-test-marker = 'Developer Tools']");
-  public SelenideElement searchButton = $("[data-test='site-header-search-action']");
+    @FindBy(xpath = "//*[@data-test-marker='Developer Tools']")
+    public WebElement seeDeveloperToolsButton;
+
+    @FindBy(css = "[data-test='suggestion-action-link']")
+    public WebElement findYourToolsButton;
+
+    @FindBy(xpath = "//div[@data-test='main-menu-item' and @data-test-marker = 'Developer Tools']")
+    public WebElement toolsMenu;
+
+    @FindBy(css = "[data-test='site-header-search-action']")
+    public WebElement searchButton;
+
+    public MainPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
 }
